0e93c23b5fb2af10cd7d44951b8464c7351c9a61
Jonas Garcia Rosa <jgarcia.rosa@gmail.com>
2023-11-21 16:12:15 -0300

    initial config

---
+++ src/main/java/br/com/jgarciarosa/restpringbootjava/RestPringBootJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.jgarciarosa.restpringbootjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPringBootJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestPringBootJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jgarciarosa/restpringbootjava/RestPringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jgarciarosa.restpringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
