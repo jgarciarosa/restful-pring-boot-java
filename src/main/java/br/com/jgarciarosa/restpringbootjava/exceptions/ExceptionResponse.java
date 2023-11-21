eb0a24f2b99b0b840df54af4f326fc17ec2b8e8f
Jonas Garcia Rosa <jgarcia.rosa@gmail.com>
2023-11-21 20:41:31 -0300

    creating person services, and custom exceptions

---
+++ src/main/java/br/com/jgarciarosa/restpringbootjava/exceptions/ExceptionResponse.java
@@ -0,0 +1,29 @@
+package br.com.jgarciarosa.restpringbootjava.exceptions;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+    private final Date timeStamp;
+    private final String message;
+    private final String details;
+
+    public ExceptionResponse(Date timeStamp, String message, String details) {
+        this.timeStamp = timeStamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
---
+++ src/main/java/br/com/jgarciarosa/restpringbootjava/services/PersonServices.java
@@ -0,0 +1,61 @@
+package br.com.jgarciarosa.restpringbootjava.services;
+
+import br.com.jgarciarosa.restpringbootjava.data.models.Person;
+import br.com.jgarciarosa.restpringbootjava.exceptions.ResourceNotFoundException;
+import br.com.jgarciarosa.restpringbootjava.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    @Autowired
+    PersonRepository repository;
+
+    Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findBtId(Long id) {
+
+        logger.info("Encontrando uma pessoa!");
+
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("ID não encontrado!"));
+    }
+
+    public List<Person> findAll() {
+
+        logger.info("Encontrando todas pessoas!");
+
+        return repository.findAll();
+    }
+
+    public Person create(Person person) {
+
+        logger.info("Criando uma pessoa!");
+
+        return repository.save(person);
+    }
+
+    public Person update(Person person) {
+
+        logger.info("Atualizando uma pessoa!");
+        Person entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException(
+                "Id não encontrado!"));
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setGender(person.getGender());
+
+        return repository.save(person);
+    }
+
+    public void delete(Long id) {
+
+        logger.info("Deletando uma pessoa!");
+        Person entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+                "Id não encontrado!"));
+
+        repository.delete(entity);
+    }
+}
