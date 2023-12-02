328dcfc99b4aeffb52c79af3f4b4b83514cd5804
Jonas Garcia Rosa <jgarcia.rosa@gmail.com>
2023-12-02 17:16:20 -0300

    updating endpoint with version, and defining custom JSON Serialization

--- src/main/java/br/com/jgarciarosa/restpringbootjava/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/jgarciarosa/restpringbootjava/data/vo/v1/PersonVO.java
@@ -1,5 +1,7 @@
 package br.com.jgarciarosa.restpringbootjava.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +13,15 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode(of = "id")
+@JsonPropertyOrder({"person_id", "person_gender", "person_first_name", "person_last_name"})
 public class PersonVO implements Serializable {
 
+    @JsonProperty("person_id")
     private Long id;
+    @JsonProperty("person_first_name")
     private String firstName;
+    @JsonProperty("person_last_name")
     private String lastName;
+    @JsonProperty("person_gender")
     private String gender;
 }
